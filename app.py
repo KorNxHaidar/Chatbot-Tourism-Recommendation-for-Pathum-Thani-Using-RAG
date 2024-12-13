0889a9fd52ad06131b8de3bf86de1d1ea6139a0a
Sirichupong Jumpasak <93991135+KorNxHaidar@users.noreply.github.com>
2024-12-13 23:49:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,6 @@ from rag_variables import (
 
 dotenv.load_dotenv()
 
-typhoon_token = os.getenv("TYPHOON_API_KEY")
-client = OpenAI(
-    api_key=typhoon_token,
-    base_url="https://api.opentyphoon.ai/v1" 
-)
 
 # Set up Streamlit
 st.set_page_config(
@@ -84,22 +79,66 @@ def answer_question(user_question, chat_history):
     response = generate_response(context=context, chat_history=chat_history, question=user_question)
     return response 
 
+# --- Side Bar LLM API Tokens ---
+with st.sidebar:
+    st.write("Setting API KEY")
+    if "TYPHOON_API_KEY" not in os.environ:
+        default_typhoon_api_key = os.getenv("TYPHOON_API_KEY") if os.getenv("TYPHOON_API_KEY") is not None else ""  # only for development environment, otherwise it should return None
+        with st.popover("🔐 TYPHOON API KEY"):
+            typhoon_api_key = st.text_input(
+                "Introduce your Typhoon API Key (https://opentyphoon.ai/app/api-key)", 
+                value=default_typhoon_api_key, 
+                type="password",
+                key="typhoon_api_key",
+            )
+        typhoon_token = typhoon_api_key
+        client = OpenAI(
+            api_key=typhoon_token,
+            base_url="https://api.opentyphoon.ai/v1" 
+        )    
+    else:
+        typhoon_api_key = os.getenv("TYPHOON_API_KEY")
+        st.session_state.typhoon_api_key = typhoon_api_key
+        typhoon_token = typhoon_api_key
+        client = OpenAI(
+            api_key=typhoon_token,
+            base_url="https://api.opentyphoon.ai/v1" 
+        )
+
 # --- Main Chat App ---
-# Handle incoming user input and chat history
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# Checking if the user has introduced the OpenAI API Key, if not, a warning is displayed
+missing_openai = typhoon_api_key == "" or typhoon_api_key is None or "sk-" not in typhoon_api_key
+if missing_openai and ("TYPHOON_API_KEY" not in os.environ):
+    st.write("##")
+    st.warning("⬅️ Please introduce an API Key to continue...")
+else:
+    # Handle incoming user input and chat history
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-# Main chat flow where the assistant's response will be shown with type effect
-if prompt := st.chat_input("Message Typhoon Chat"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+    # Main chat flow where the assistant's response will be shown with type effect
+    if prompt := st.chat_input("Message Typhoon Chat"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
 
-        messages = [{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]
-        full_response = generate_response(prompt, messages, prompt, message_placeholder)
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
+            messages = [{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]
+            full_response = generate_response(prompt, messages, prompt, message_placeholder)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+with st.sidebar:
+    st.divider()
+    st.write("Tools")
+    st.write("![Python](https://img.shields.io/badge/python-3775A9?logo=pypi&logoColor=ffdd54)" ,"![PyMu](https://img.shields.io/badge/PyPI-3775A9?logo=pypi&logoColor=fff)", "![Pandas](https://img.shields.io/badge/Pandas-150458?logo=pandas&logoColor=fff)","![FAISS](https://img.shields.io/badge/FAISS-%230467DF?style=plastic&logo=meta&logoColor=%230467DF&color=white)")
+    st.write("![LangChain](https://img.shields.io/badge/LangChain-%231C3C3C?style=plastic&logo=langchain&logoColor=white&color=%231C3C3C)", "[![Hugging Face](https://img.shields.io/badge/Hugging%20Face-FFD21E?logo=huggingface&logoColor=000)](https://huggingface.co/BAAI/bge-m3)")
+    st.write("[![typhoon v1.5x 70b instruct](https://img.shields.io/badge/typhoon%20v1.5x%2070b%20instruct-%23000000?style=plastic&logo=ollama&logoColor=white&color=%23726bdf)](https://opentyphoon.ai/)")
+
+    st.divider()
+    st.write("More details")
+    st.write("[![github](https://img.shields.io/badge/github-%23181717?style=flat&logo=github&color=000&)](https://github.com/KorNxHaidar/Chatbot-Tourism-Recommendation-for-Pathum-Thani-Using-RAG)", "[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/drive/1wLfRtjCxXKz7qgiACqqggyRNmMxiBa_0#scrollTo=PIFGIn5EJlE6)")
+    st.write("[![View Project Docs](https://img.shields.io/badge/View-Project_Docs-green?style=flat)](https://colab.research.google.com/drive/1wLfRtjCxXKz7qgiACqqggyRNmMxiBa_0#scrollTo=PIFGIn5EJlE6)")
